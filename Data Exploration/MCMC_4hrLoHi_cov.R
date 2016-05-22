5e03ab06ea401290f9270c37a298be1d38fc089d
MLBartley <docbartley@gmail.com>
2016-05-22 12:48:24 -0400

    Ants Project Organization
    Been thinking about how to organize this project better. Updates are
    mostly reorganizing and renaming of data.

--- Data Exploration/MCMC_4hrLoHi_cov.R
+++ Data Exploration/MCMC_4hrLoHi_cov.R
@@ -17,3 +17,24 @@
 #how can it be improved?
 
 
+#high density trophallaxis data
+
+troph.high.4 = read.csv("./Data/Colony1_trophallaxis_high_density_4hr.csv")
+troph.low.4 = read.csv("./Data/Colony1_trophallaxis_low_density_4hr.csv")
+
+
+#in and out data 
+
+inout.high.4 = read.csv("./Data/Colony1_in&out_high_density_4hr.csv")
+#only want entrances
+inout.high.4 = inout.high.4[which(inout.high.4$Action == "enter"),]
+
+inout.low.4 = read.csv("./Data/Colony1_in&out_low_density_4hr.csv")
+#onlly want entrances
+inout.low.4 = inout.low.4[which(inout.low.4$Action == "Enter"),]
+
+
+#visualize high data
+
+
+
