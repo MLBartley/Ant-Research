9db5b571b8c609a829203022e130ddf7549a532e
MLBartley <docbartley@gmail.com>
2016-06-14 12:56:55 -0400

    New prep function
    N_t now number of ants in trophallaxis at time t.

--- R/mcmc.troph_test.R
+++ R/mcmc.troph_test.R
@@ -1,4 +1,4 @@
-lambda = c(0, .05)
+lambda = c(.05, 1)
 
 P = matrix(c(.8, .2, .2, .8), nrow = 2, byrow = T)
 sim = sim.mmpp(tmax = 2 * 60 * 60, delta.t = 30, 
@@ -9,7 +9,7 @@ sim = sim.mmpp(tmax = 1 * 60 * 60, delta.t = 1,
                start.state = 1, P = P, lambda = lambda)
 ####
 
-delta.t = 30
+delta.t = 60
 sum(sim$y)
 
 sim$start_time = sim$t[which(sim$y >= 1)] #only works with per second data
@@ -26,6 +26,13 @@ for(i in 1:length(sim$intbin)){
   tint = tint + delta.t
 }
 
+#######
+# what if instead of binning by totaling interactions, we instead average?
+
+sim$average = sim$intbin / delta.t
+
+
+
 ####
 theta = matrix(data = c(90, 10, 10, 90), nrow = 2, ncol = 2, byrow = T) 
 
@@ -34,7 +41,22 @@ test = mcmc.troph(y.data = sim$y, ant.file = sim,
                   theta = theta, states = 2, n.mcmc = 2000,
                   delta.t = 1, hours = 1)
 
-test.bin = mcmc.troph(y.data = sim$y, ant.file = sim,
-                      title = "test", a = 2, b = 2, 
-                      theta = theta, states = 2, n.mcmc = 2000,
-                      delta.t = 30, hours = 1)
+test.bin5 =  mcmc.troph(y.data = sim$intbin, ant.file = sim,
+                        title = "test", a = .1, b = 2, 
+                        theta = theta, states = 2, n.mcmc = 2000,
+                        delta.t = 5, hours = 1)
+
+# test.bin30 = mcmc.troph(y.data = sim$intbin, ant.file = sim,
+#                       title = "test", a = .1, b = 2, 
+#                       theta = theta, states = 2, n.mcmc = 2000,
+#                       delta.t = 30, hours = 1)
+ 
+ test.bin60 = mcmc.troph(y.data = sim$intbin, ant.file = sim,
+                       title = "test", a = .1, b = 2,
+                       theta = theta, states = 2, n.mcmc = 2000,
+                       delta.t = 60, hours = 1)
+
+# test.avg = mcmc.troph(y.data = sim$average, ant.file = sim,
+#                       title = "test", a = .1, b = 2, 
+#                       theta = theta, states = 2, n.mcmc = 2000,
+#                       delta.t = 5, hours = 1)
