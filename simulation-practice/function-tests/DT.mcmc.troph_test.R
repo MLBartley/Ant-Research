b0b4f870ae81d212b69d0d54607af110ca7dd621
MLBartley <docbartley@gmail.com>
2016-09-02 11:00:02 -0400

    update so i can retest dt.mcmc.troph function

--- simulation-practice/function-tests/DT.mcmc.troph_test.R
+++ simulation-practice/function-tests/DT.mcmc.troph_test.R
@@ -30,39 +30,41 @@ sim30 = sim.DT.troph(tmax = 1 * 60 * 60, delta.t = 30,
 #
 # 
 # y.data = sim30$inter.persec
-# ant.file = sim30
-# title = "test"
-# a = .1
-# b = .5
-# c = .1
-# d = .5
-# theta = theta
-# states = 2
-# n.mcmc = 2000
+y.data = sim30$bin.inter
+ant.file = sim30
+title = "test"
+a = .1
+b = .5
+c = .1
+d = .5
+theta = theta
+states = 2
+n.mcmc = 2000
 # delta.t = 1
-# hours = 1
-# 
-# data = sim30$inter.persec
-# Time = length(data)
-# n = 2
-# delta = rep(1/n, n)
-# 
-# #needed for final graphic
-# location = sim30$location
-# start = sim30$start.time
-# start = sort(start)
-# int.num = length(start)
-# maxtime = hours * 60 * 60
-# 
+delta.t = 30
+hours = 1
+
+data = y.data
+Time = length(data)
+n = 2
+delta = rep(1/n, n)
+
+#needed for final graphic
+location = sim30$location
+start = sim30$start_time
+start = sort(start)
+int.num = length(start)
+maxtime = hours * 60 * 60
+
 
 
 #fit simulated date using function
 
-theta = matrix(data = c(90, 10, 10, 90), nrow = 2, ncol = 2, byrow = T) 
+theta = matrix(data = c(500, 10, 10, 500), nrow = 2, ncol = 2, byrow = T) 
 
 test.bin1 = DT.mcmc.troph(y.data = sim30$inter.persec, ant.file = sim30,
                   title = "test", a = .1, b = 2, c = .1, d = .5, 
-                  theta = theta, states = 2, n.mcmc = 10,
+                  theta = theta, states = 2, n.mcmc = 5000,
                   delta.t = 1, hours = 1)
 
 # test.bin5 =  mcmc.troph(y.data = sim$intbin, ant.file = sim,
@@ -70,8 +72,8 @@ test.bin1 = DT.mcmc.troph(y.data = sim30$inter.persec, ant.file = sim30,
 #                         theta = theta, states = 2, n.mcmc = 2000,
 #                         delta.t = 5, hours = 1)
 
-test.bin30 = mcmc.troph(y.data = sim$intbin, ant.file = sim,
-                      title = "test", a = .1, b = 2,
+test.bin30 = DT.mcmc.troph(y.data = sim30$intbin, ant.file = sim30,
+                      title = "test", a = .1, b = 2, c = .1, d = .5,
                       theta = theta, states = 2, n.mcmc = 2000,
                       delta.t = 30, hours = 1)
 # 
