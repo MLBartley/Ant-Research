be5452c997d7c801b47c380c8ecba5e93db8af63
MLBartley <docbartley@gmail.com>
2016-11-26 11:39:53 -0500

    update

--- simulation-practice/DT.pen.troph-Sim-Script.R
+++ simulation-practice/DT.pen.troph-Sim-Script.R
@@ -37,7 +37,7 @@ start = list(X = X, lambda = lambda, gamma = gamma)
 
 #apply funciton to penalty parameters
 #
-n.mcmc = 20000
+n.mcmc = 10000
 seconds = 1
 
 results = lapply(penalty, FUN = DT.pen.mcmc.troph, y.data = sim$inter.persec, states = 2,
@@ -134,6 +134,10 @@ table = data.frame(c(0,penalty), lambda.low.est, lambda.high.est,
 write.csv(x = table, file = paste("./output/", Sys.time(), ".csv", sep = "") )
 
 
+
+
+
+
 start.30 = list(X = X.30, lambda = lambda, gamma = gamma)
 seconds = 30
 
@@ -142,31 +146,30 @@ results.30 = lapply(penalty, FUN = DT.pen.mcmc.troph, y.data = sim$bin.inter, st
                  a = .08, b = .005, c = .08, d = .005,
                  n.mcmc = n.mcmc, seconds = 30, fig.save = T, start = start.30)
 
-results = results.30
 
 
 lambda.low.est = lambda[1] * seconds
 
 for(i in 1:length(penalty)){
-  lambda.low.est = c(lambda.low.est, results[[i]]$lambda.est[[1]]$est)
+  lambda.low.est = c(lambda.low.est, results.30[[i]]$lambda.est[[1]]$est)
 }
 
 lambda.high.est = lambda[2] * seconds
 
 for(i in 1:length(penalty)){
-  lambda.high.est = c(lambda.high.est, results[[i]]$lambda.est[[2]]$est)
+  lambda.high.est = c(lambda.high.est, results.30[[i]]$lambda.est[[2]]$est)
 }
 
 gamma.low.est = 0.005
 
 for(i in 1:length(penalty)){
-  gamma.low.est = c(gamma.low.est, results[[i]]$gamma.est[[1]]$est)
+  gamma.low.est = c(gamma.low.est, results.30[[i]]$gamma.est[[1]]$est)
 }
 
 gamma.high.est = 0.005
 
 for(i in 1:length(penalty)){
-  gamma.high.est = c(gamma.high.est, results[[i]]$gamma.est[[2]]$est)
+  gamma.high.est = c(gamma.high.est, results.30[[i]]$gamma.est[[2]]$est)
 }
 
 
@@ -174,38 +177,49 @@ for(i in 1:length(penalty)){
 P.11.est = P30[1, 1]
 
 for(i in 1:length(penalty)){
-  P.11.est = c(P.11.est, results[[i]]$P.est[[1]]$est)
+  P.11.est = c(P.11.est, results.30[[i]]$P.est[[1]]$est)
 }
 
 P.12.est = P30[1, 2]
 
 for(i in 1:length(penalty)){
-  P.12.est = c(P.12.est, results[[i]]$P.est[[2]]$est)
+  P.12.est = c(P.12.est, results.30[[i]]$P.est[[2]]$est)
 }
 
 P.21.est = P30[2, 1]
 
 for(i in 1:length(penalty)){
-  P.21.est = c(P.21.est, results[[i]]$P.est[[3]]$est)
+  P.21.est = c(P.21.est, results.30[[i]]$P.est[[3]]$est)
 }
 
 P.22.est = P30[2, 2]
 
 for(i in 1:length(penalty)){
-  P.22.est = c(P.22.est, results[[i]]$P.est[[4]]$est)
+  P.22.est = c(P.22.est, results.30[[i]]$P.est[[4]]$est)
 }
 
 
 MSPE.est = 0
 
 for(i in 1:length(penalty)){
-  MSPE.est = c(MSPE.est, results[[i]]$MSPE)
+  MSPE.est = c(MSPE.est, results.30[[i]]$MSPE)
 }
 
 
 accept = n.mcmc
 
 for(i in 1:length(penalty)){
-  accept = c(accept, results[[i]]$accept)
+  accept = c(accept, results.30[[i]]$accept)
 }
+
+table = data.frame(c(0,penalty), lambda.low.est, lambda.high.est, 
+                   gamma.low.est, gamma.high.est,
+                   P.11.est, P.12.est, P.21.est, P.22.est, 
+                   MSPE.est, accept)
+
+rownames(table) <- c("Truth", "Model 1", "Model 2", "Model 3", 
+                     "Model 4", "Model 5", "Model 6", "Model 7", 
+                     "Model 8", "Model 9", "Model 10")
+write.csv(x = table, file = paste("./output/", Sys.time(), ".csv", sep = "") )
+
 
