a80c1a3b9da2bcc32ea19656bba144a15a2a2d98
MLBartley <docbartley@gmail.com>
2017-12-18 15:08:47 -0500

    merging with E machine

--- Ants/R/diagnoses_functions.R
+++ Ants/R/diagnoses_functions.R
@@ -1,100 +1,173 @@
 
-#' Diagnoses of Penalized HMM Model 
+#' Diagnoses of Penalized HMM Model
 #'
-#' @param mcmc_matrix 
+#' @param mcmc_matrix
+#' @param states
+#' @param ant_file
+#' @param chamber
 #' @param Time
 #' @param fig_path
 #' @param fig_name
 #' @param penalty
 #'
-#' 
+#'
 #'
 #' @return
 #' @export
 #'
 
 
-penalty_diagnosis <- function(mcmc_matrix, Time, fig_path, fig_name, penalty){
-  
-  
-  
-  
-  
+penalty_diagnosis <- function(mcmc_matrix, states, ant_file, chamber, Time, fig_path, fig_name, penalty){
+
+
+n <- states
+
+
   source("http://www.stat.psu.edu/~mharan/batchmeans.R")
-  
+
   #batch means (mean, s.error)
-  bmeans <- bmmat(t(mcmc_matrix[-(Time+10:nrow(mcmc_matrix))]))
-  
-pdf( file = paste(fig_path, fig_name, round(penalty, 11), ".diagnostics", ".pdf", sep = ""))  
+  bmeans <- bmmat(t(mcmc_matrix))
+
+pdf( file = paste(fig_path, fig_name, round(penalty, 11), ".diagnostics", ".pdf", sep = ""))
   #estimates vs sample size
-  
+
     #lamdas - trop.rate(L/H)
-    
-    par(mfrow = c(1, 2))  
-    
+
+    par(mfrow = c(1, 2))
+
     estvssamp(samp = mcmc_matrix[1, ], plotname = "Trophallaxis Rate: Low")
-    estvssamp(samp = (mcmc_matrix[1, ] + mcmc_matrix[2, ]), plotname = "Trophallaxis Rate: High")
-    
+
+    if (n == 2) {
+      estvssamp(samp = (mcmc_matrix[1, ] + mcmc_matrix[2, ]), plotname = "Trophallaxis Rate: High")
+    }else{
+      estvssamp(samp = (mcmc_matrix[1, ] + mcmc_matrix[2, ]),
+        plotname = "Trophallaxis Rate: Medium")
+      estvssamp(samp = (mcmc_matrix[1, ] + mcmc_matrix[2, ] + mcmc_matrix[3, ]),
+        plotname = "Trophallaxis Rate: High")
+    }
+
+
     #gammas - st_switch_rate(LH/HL)
-    
-    estvssamp(samp = mcmc_matrix[4, ], plotname = "State Switching Rate: Low to High")
+    if (n == 2) {
+      estvssamp(samp = mcmc_matrix[4, ], plotname = "State Switching Rate: Low to High")
     estvssamp(samp = mcmc_matrix[5, ], plotname = "State Switching Rate: High to Low")
-    
+    }else{
+      estvssamp(samp = mcmc_matrix[5, ], plotname = "State Switching Rate: Low to High")
+      estvssamp(samp = mcmc_matrix[6, ], plotname = "State Switching Rate: High to Low")
+      estvssamp(samp = mcmc_matrix[7, ], plotname = "State Switching Rate: Medium to Low")
+      estvssamp(samp = mcmc_matrix[8, ], plotname = "State Switching Rate: Medium to High")
+      estvssamp(samp = mcmc_matrix[9, ], plotname = "State Switching Rate: Low to Medium")
+      estvssamp(samp = mcmc_matrix[10, ], plotname = "State Switching Rate: High to Medium")
+    }
+
     #Probabilities
-    
+
     par(mfrow = c(2, 2))
-    
+if (n == 2) {
     estvssamp(samp = mcmc_matrix[6, ], plotname = "Probability Transitions: Low to Low")
-    estvssamp(samp = mcmc_matrix[7, ], plotname = "State Switching Rate: Low to High")
-    estvssamp(samp = mcmc_matrix[8, ], plotname = "State Switching Rate: High to Low")
-    estvssamp(samp = mcmc_matrix[9, ], plotname = "State Switching Rate: High to High")
-    
-    
-    #Random time for state chain - 
+    estvssamp(samp = mcmc_matrix[7, ], plotname = "Probability Transitions: Low to High")
+    estvssamp(samp = mcmc_matrix[8, ], plotname = "Probability Transistions: High to Low")
+    estvssamp(samp = mcmc_matrix[9, ], plotname = "Probability Transistions: High to High")
+}else{
+  estvssamp(samp = mcmc_matrix[11, ], plotname = "Probability Transitions: Low to Low")
+  estvssamp(samp = mcmc_matrix[12, ], plotname = "Probability Transitions: Low to Medium")
+  estvssamp(samp = mcmc_matrix[13, ], plotname = "Probability Transitions: Low to High")
+
+  estvssamp(samp = mcmc_matrix[14, ], plotname = "Probability Transitions: Medium to Low")
+  estvssamp(samp = mcmc_matrix[15, ], plotname = "Probability Transitions: Medium to Medium")
+  estvssamp(samp = mcmc_matrix[16, ], plotname = "Probability Transitions: Medium to High")
+
+  estvssamp(samp = mcmc_matrix[17, ], plotname = "Probability Transitions: High to Low")
+  estvssamp(samp = mcmc_matrix[18, ], plotname = "Probability Transitions: High to Medium")
+  estvssamp(samp = mcmc_matrix[19, ], plotname = "Probability Transitions: High to High")
+
+}
+
+
+
+    #Random time for state chain -
 
     par(mfrow = c(1, 1))
-    
+
     sample_x <- sample(x = 10:nrow(mcmc_matrix), 1)
+    if (n != 2) { sample_x <- sample(20:nrow(mcmc_matrix), 1)}
     estvssamp(samp = mcmc_matrix[sample_x, ], plotname = "Behavior State: Random Time Chain")
-    
-  
+
+
   # visualization
-    
-  #lambdas - troph.rate  
-  plot(coda::mcmc(t(mcmc_matrix[1:2, ])))
-  
+
+  #lambdas - troph.rate
+    if (n == 2) {
+      plot(coda::mcmc(t(mcmc_matrix[1:2, ])))
+
   #gammas - st_switch_rate
   plot(coda::mcmc(t(mcmc_matrix[4:5, ])))
-  
-  #ptm 
+
+  #ptm
   plot(coda::mcmc(t(mcmc_matrix[6:9, ])))
-  
+
   #rounded estimates over time for sto process
-  
-   plot(round(bmeans[10:(10+Time),1 ]), type = "l")
-    
+
+   plot(round(bmeans[-(1:9), 1]), type = "l")
+    }else{
+      plot(coda::mcmc(t(mcmc_matrix[1:3, ])))
+
+      #gammas - st_switch_rate
+      plot(coda::mcmc(t(mcmc_matrix[5:10, ])))
+
+      #ptm
+      plot(coda::mcmc(t(mcmc_matrix[11:19, ])))
+
+      #rounded estimates over time for sto process
+
+      plot(round(bmeans[-(1:19), 1]), type = "l")
+    }
+
 
    #fancy plot
-   par(mfrow = c(1, 1))
-   
-  
-     ## High Density - 4 Hours
-     plot(start, 1:Time, xlab = "Seconds", ylab = "Cumulative Interaction Count", 
-          xlim = c(0, Time))
-     states <- bmeans[-(1:9), 1]  #from code above
-     rr <- rle(states[, 1])
-     rr$values <- round(rr$values, digits = 0)
-     embedded.chain <- rr$values
-     cs <- c(0, cumsum(rr$lengths)) * delta_t - delta_t
-     cols <- c("#bc535644", "#538bbc44")
-     for (j in 1:length(embedded.chain)) {
-       rect(cs[j], 0, cs[j + 1], int.num, col = cols[embedded.chain[j]], 
-            density = NA)
-       
+
+   # needed for final graphic
+   location <- ant_file$Location
+   start <- ant_file$start_time
+   # chamber <- chamber
+
+   if (length(unique(location)) != 1){
+
+     if (chamber == "queen") {
+       start = start[which(location == 1)]
+     }else{
+       start = start[which(location == 4)]
      }
-  
+   }
+
+   start <- sort(start)
+   int.num <- length(start)
+   maxtime <- Time
+
+
+   par(mfrow = c(1, 1))
+
+
+   ## High Density - 4 Hours
+   plot(start, 1:int.num, xlab = "Seconds", ylab = "Cumulative Interaction Count",
+     xlim = c(0, Time))
+   states <- bmeans[-(1:9), 1]  #from code above
+   if (n != 2) { states <- bmeans[-(1:19), 1]}
+   rr <- rle(states)
+   rr$values <- round(rr$values, digits = 0)
+   embedded.chain <- rr$values
+   cs <- c(0, cumsum(rr$lengths)) * delta_t - delta_t
+   cols <- c("#bc535644", "#538bbc44")
+   for (j in 1:length(embedded.chain)) {
+     rect(cs[j], 0, cs[j + 1], int.num, col = cols[embedded.chain[j]],
+       density = NA)
+
+   }
+   points(start, 1:int.num,
+     xlim = c(0, maxtime))
+
     dev.off()
-  
+
   return(bmeans)
 
 }
--- Ants/R/simulation_functions.R
+++ Ants/R/simulation_functions.R
@@ -1,161 +1,193 @@
-#' 
+#'
 #' Simulation of Ant Trophallaxis Start Times - Simple Model
-#' 
+#'
 #' \code{sim_DT_troph} simulates number of ant interactions
 #' starting per second and per delta_t seconds.
-#' 
+#'
 #' This function allows you to simulate data from the following
 #' model: X_t ~ Markov Chain ( X_t-1 , P ) [two states] N_t ~
 #' Pois (lambda_L + lambda_Change) lambda_L ~ Gam(a, b)
 #' lambda_Change ~ Gam(c, d) lambda_H = lambda_l + lambda_Change
-#' 
+#'
+#' @param states Number of states for the feeding interactions.
 #' @param time_max Number of seconds for which to simulate data.
-#' @param delta_t Length of interval to bin interaction and state 
+#' @param delta_t Length of interval to bin interaction and state
 #'   data into.
 #' @param start_state Starting trophallaxis rate state (X_1 in model
 #'   write - up). Defaults to state 1 (low).
-#' @param switch_rate Rate at which the rates at which ants trophallax 
-#'   switch (between high and low). Denoted by gamma in model write-up. 
+#' @param switch_rate Rate at which the rates at which ants trophallax
+#'   switch (between high and low). Denoted by gamma in model write-up.
 #'   Used to calculate probability transition matrix (P).
-#' @param state_tpm Probably transition matrix for switching between 
-#'   trophallaxis states (denoted by P in write-up). 
-#' @param int_rate Rate of starting interactions. Denoted by lambda in 
+#' @param state_tpm Probably transition matrix for switching between
+#'   trophallaxis states (denoted by P in write-up).
+#' @param int_rate Rate of starting interactions. Denoted by lambda in
 #'   model write-up).
-#' @param num_locations Number of chambers/locations in which ants are 
+#' @param num_locations Number of chambers/locations in which ants are
 #'   found trophallaxis.
-#'   
+#'
 #' @return This function will return the following:
 #'   \enumerate{
 #'     \item inter_persec: (0, ...) 'observed' number of
 #'       interactions per 1 second
 #'     \item state: (1 = low, 2 = high) unobserved two state process
 #'     \item cumu_inter: (0, ...) cumulative count of interactions
-#'     \item bin_inter: Trophallaxis interactions binned into smaller 
+#'     \item bin_inter: Trophallaxis interactions binned into smaller
 #'       intervals determined by sum over delta_t
-#'     \item bin_state: Trophallaxis rate state binned into smaller 
+#'     \item bin_state: Trophallaxis rate state binned into smaller
 #'       intervals, determined by average over delta_t
 #'     \item bin_sec: (0, ...) time (in seconds) binned by delta_t
 #'     \item start_time: vector of interaction start times
 #'     \item location: location of each ant interaction
-#'     \item Visuals: 1x1 visual of cumlative counts over time, colored 
+#'     \item Visuals: 1x1 visual of cumlative counts over time, colored
 #'       by state, separated by location if applicable.
 #'   }
-#'  
-#'   
-#'   
+#'
+#'
+#'
 #' @keywords simulation, HMM, Poisson Process
 #' @export
-#' @examples 
+#' @examples
 #' P <- matrix(c(.99, .01, .01, .99), nrow = 2, byrow = T)
 #' lambda <- k <- c(1, 4)
-#' delta_t <- 5 
-#' sim <- sim.DT.troph(time_max = 3600, delta_t = delta_t, 
-#'                     start_state = 1, switch_rate = 0, state_tpm = P, 
+#' delta_t <- 5
+#' sim <- sim.DT.troph(time_max = 3600, delta_t = delta_t,
+#'                     start_state = 1, switch_rate = 0, state_tpm = P,
 #'                     int_rate = lambda, num_locations = 1)
-#' 
+#'
 
-sim_DT_troph <- function(time_max, delta_t, start_state = 1, 
+sim_DT_troph <- function(states, time_max, delta_t, start_state = 1,
                         switch_rate = c(0, 0), state_tpm, int_rate,
                         num_locations = 1) {
-    
-  #if given switch rates (gamma in model) use them to calculate transition 
-  #probability matrices 
-  
+
+  #if given switch rates (gamma in model) use them to calculate transition
+  #probability matrices
+  if( states == 2){
+
    if (switch_rate[1] == 0) {
         state_tpm <- state_tpm
     } else {
-        state_tpm[1, 2] <- switch_rate[1] * exp(-switch_rate[1] * 1) / 
+        state_tpm[1, 2] <- switch_rate[1] * exp(-switch_rate[1] * 1) /
           (exp(1) / (exp(1) - 1)^2)
         state_tpm[1, 1] <- 1 - state_tpm[1, 2]
-        state_tpm[2, 1] <- switch_rate[2] * exp(-switch_rate[2] * 1) / 
+
+        state_tpm[2, 1] <- switch_rate[2] * exp(-switch_rate[2] * 1) /
           (exp(1) / (exp(1) - 1)^2)
         state_tpm[2, 2] <- 1 - state_tpm[1, 2]
     }
-    
-    
-    #create homes for simulated states and interactions 
-  
+  }else{
+    if (switch_rate[1] == 0) {
+      state_tpm <- state_tpm
+    } else {
+      state_tpm[1, 2] <- switch_rate[5] * exp(-switch_rate[5] * delta_t) /
+        (exp(1) / (exp(1) - 1)^2) #LM
+
+      state_tpm[1, 3] <- switch_rate[2] * exp(-switch_rate[2] * delta_t) /
+        (exp(1) / (exp(1) - 1)^2) #LH
+
+      state_tpm[1, 1] <- 1 - state_tpm[1, 2] - state_tpm[1, 3] #LL
+
+      state_tpm[2, 1] <- switch_rate[3] * exp(-switch_rate[3] * delta_t) /
+        (exp(1) / (exp(1) - 1)^2) #ML
+
+      state_tpm[2, 3] <- switch_rate[4] * exp(-switch_rate[4] * delta_t) /
+        (exp(1) / (exp(1) - 1)^2) #MH
+
+      state_tpm[2, 2] <- 1 - state_tpm[2, 1] - state_tpm[2, 3] #MM
+
+      state_tpm[3, 1] <- switch_rate[2] * exp(-switch_rate[2] * delta_t) /
+        (exp(1) / (exp(1) - 1)^2) #HL
+
+      state_tpm[3, 2] <- switch_rate[6] * exp(-switch_rate[6] * delta_t) /
+        (exp(1) / (exp(1) - 1)^2) #HM
+
+      state_tpm[3, 3] <- 1 - state_tpm[3, 1] - state_tpm[3, 2] #HH
+    }
+  }
+
+
+    #create homes for simulated states and interactions
+
     state <- rep(NA, time_max)
     interactions <- rep(NA, time_max)
     state[1] <- start_state
     interactions[1] <- 0
-    
+
     for (t in 2:time_max) {
-        
-        
+
+
         ## sample latent state (low, high)
-        state[t] <- sample(1:2, 1, prob = state_tpm[state[t - 1], ])
-        
+        state[t] <- sample(1:states, 1, prob = state_tpm[state[t - 1], ])
+
         ## sample observed events (number of interactions started at time t)
-        
+
         interactions[t] <- rpois(1, lambda = int_rate[state[t]] * 1)
         # want to simulate data every second, but then bin by delta_t
         # aftwards
-        
+
     }
-    
+
     t <- (0:(time_max - 1))  #vector of seconds through time_max
-     
-    
+
+
     # bin number of interactions started within delta_t time intervals
 
-    bin_inters <- rep(NA, length(interactions)/delta_t) 
-    
+    bin_inters <- rep(NA, length(interactions)/delta_t)
+
     tintv <- 1
-    
-    
+
+
     for (i in 1:length(bin_inters)) {
-        
+
         bin_inters[i] <- sum(interactions[tintv:(tintv + delta_t - 1)])
         tintv <- tintv + delta_t
     }
-    
+
     #bin trophallaxis states within delta_t time intervals
-    
+
     bin_state <- rep(NA, length(bin_inters))
-    
+
     tintv <- 1
-    
-  
+
+
     for (i in 1:length(bin_state)) {
         bin_state[i] <- round(mean(state[tintv:(tintv + delta_t - 1)]))
         tintv <- tintv + delta_t
     }
-    
-    
-    
+
+
+
     start_time <- t[which(interactions >= 1)]
-    
+
     #Visualize simulated ant interaction data - POSSIBLY PULL INTO VISUAL FUNCTION LATER
-    
+
     par(mfrow = c(1, 1))
-    
-    plot(1:(length(cumsum(interactions))), cumsum(interactions), 
-        type = "p", pch = ".", 
-        cex = 2, col = state, xlab = "Seconds", 
-        ylab = "Cumulative Interactions", 
+
+    plot(1:(length(cumsum(interactions))), cumsum(interactions),
+        type = "p", pch = ".",
+        cex = 2, col = state, xlab = "Seconds",
+        ylab = "Cumulative Interactions",
         main = "Simulated Data")
-    
+
     plot(1:time_max, interactions, type = "l", cex = 2, col = state,
-        xlab = "Seconds", ylab = "Number of Interactions", 
+        xlab = "Seconds", ylab = "Number of Interactions",
         main = "Simulated Data")
-    
+
     #simulate location (1 = queen's chamber, 4 = entrance)
-    
+
     if (num_locations == 1) {
         location <- rep(1, length(start_time))
     } else {
-        location <- sample(c(1, 4), size = length(start_time), replace = T, 
+        location <- sample(c(1, 4), size = length(start_time), replace = T,
             prob <- c(0.5, 0.5))
     }
-    
-    
-    
-    list(inter_persec = interactions, state = state, 
-        cumu_inter = cumsum(interactions),  
-        bin_inter = bin_inters, 
-        bin_state = bin_state, 
-        bin_sec = (0:(T - 1)) * delta_t, 
+
+
+
+    list(inter_persec = interactions, state = state,
+        cumu_inter = cumsum(interactions),
+        bin_inter = bin_inters,
+        bin_state = bin_state,
+        bin_sec = (0:(T - 1)) * delta_t,
         start_time = start_time, Location = location)
-    
+
 }
