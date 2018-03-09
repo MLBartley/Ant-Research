8cef8582d49f0f1e1c64d16607eb565940cbae22
MLBartley <docbartley@gmail.com>
2018-03-09 17:37:36 -0500

    fixed file

--- Ants/R/Col2_covariate_chamberQ.R
+++ Ants/R/Col2_covariate_chamberQ.R
@@ -1,18 +1,11 @@
 # library(magrittr)
 # library(dplyr)
-<<<<<<< HEAD
-# 
-# 
-=======
-#
-#
->>>>>>> f50ac8bd459e9f633b8384576b7f5c0b3aff848a
+
 # load("./data-raw/c2l.list.20170608.Rdata")
 # ls()
 # str(c2l.list[[1]])
 # load("./data-raw/nest.poly.ld.Rdata")
 # plot(nest.poly.ld, type = "l")
-<<<<<<< HEAD
 # 
 # #single ant's points
 # points(c2l.list[[34]][, -1])
@@ -27,7 +20,7 @@
 # times.enter.chamber = integer()
 # time.since.out = integer()
 # 
-=======
+
 #
 # #single ant's points
 # points(c2l.list[[34]][, -1])
@@ -42,7 +35,6 @@
 # times.enter.chamber = integer()
 # time.since.out = integer()
 #
->>>>>>> f50ac8bd459e9f633b8384576b7f5c0b3aff848a
 # for (antnum in 1:length(c2l.list)) {
 #   ant = c2l.list[[antnum]]
 #   # plot(nest.poly.ld, type = "l")
@@ -52,18 +44,13 @@
 #   times
 #   if (length(times) > 0) {
 #     # points(ant[times:(times + 1), -1], col = "red")
-<<<<<<< HEAD
-# 
-=======
-#
->>>>>>> f50ac8bd459e9f633b8384576b7f5c0b3aff848a
+
 #     for (i in 1:length(times)) {
 #       ant.id = c(ant.id, names(c2l.list)[[antnum]])
 #       times.enter.chamber = c(times.enter.chamber, times[i])
 #     }
 #   }
 # }
-<<<<<<< HEAD
 # 
 # ant.id
 # times.enter.chamber
@@ -85,7 +72,6 @@
 # ant.id.out <- integer()
 # times.exit.nest <- integer()
 # 
-=======
 #
 # ant.id
 # times.enter.chamber
@@ -107,35 +93,26 @@
 # ant.id.out <- integer()
 # times.exit.nest <- integer()
 #
->>>>>>> f50ac8bd459e9f633b8384576b7f5c0b3aff848a
 # for (antnum in 1:length(c2l.list)) {
 #   ant <- c2l.list[[antnum]]
 #   T <- nrow(ant)
 #   times <- which(ant$x[-1] > 198 & ant$x[-T] < 198)
 #   times
 #   if (length(times) > 0) {
-<<<<<<< HEAD
-# 
-=======
-#
->>>>>>> f50ac8bd459e9f633b8384576b7f5c0b3aff848a
 #     for (i in 1:length(times)) {
 #       ant.id.out <- c(ant.id.out, names(c2l.list)[[antnum]])
 #       times.exit.nest <- c(times.exit.nest, times[i])
 #     }
 #   }
 # }
-<<<<<<< HEAD
 # 
 # 
 # ant.id.out
 # times.exit.nest
 # 
-=======
 #
 #
 # ant.id.out
 # times.exit.nest
 #
->>>>>>> f50ac8bd459e9f633b8384576b7f5c0b3aff848a
 # unique(ant.id.out)
