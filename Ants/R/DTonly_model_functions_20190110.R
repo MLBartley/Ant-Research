51d36b36a0f93f0ac3d202a5d11b526d1853265f
MLBartley <docbartley@gmail.com>
2019-01-10 15:20:42 -0500

    moved files around to reorganize before starting new files for fresh, DT-only model approach.

---
+++ Ants/R/DTonly_model_functions_20190110.R
@@ -0,0 +1,12 @@
+################################################################################
+##
+## Discrete Time ONLY Model Functions for Ant Trophallaxis
+##
+## Date Created: Januay 10, 2019 by Meridith Bartley
+################################################################################
+
+## Outline
+    ## 1. Penalized HMM - DT Only - Covariates Optional?
+    ## 2. Visualizations after DTOnly Model run
+    ## 3. MSPE after multiple models run
+
